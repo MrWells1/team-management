4da0a7fdf6a898761ff79baa74bf58b1c03c4bf0
Vinhlee95 <lethanhvinh95@gmail.com>
2018-02-04 20:56:45 +0200

    Created EmployeeForm and refactor

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Card, CardItem, Input, Button, DayPicker } from './commons';
+import { Card, CardItem, Button } from './commons';
+import EmployeeForm from './EmployeeForm';
 import { connect } from 'react-redux';
-import { employeeUpdate, employeeCreate } from '../actions';
+import { employeeCreate } from '../actions';
 
 class EmployeeCreate extends Component {
 
@@ -13,46 +14,21 @@ class EmployeeCreate extends Component {
    render() {
       return(
          <Card>
-            {/* Name input */}
-            <CardItem>
-               <Input
-                  placeholder="Name"
-                  value={this.props.name}
-                  onChangeText={text => this.props.employeeUpdate({ prop: 'name', value: text })}
-               />
-            </CardItem>
-
-            {/* Phone Input */}
-            <CardItem>
-               <Input 
-                  placeholder="Phone Number"
-                  keyboardType="numeric"
-                  value={this.props.phone}
-                  onChangeText={text => this.props.employeeUpdate({ prop: 'phone', value: text })}
-               />
-            </CardItem>
-
-            {/* DayPicker */}
-            <DayPicker
-               selectedValue={this.props.shift}
-               onValueChange={day => this.props.employeeUpdate({ prop: 'shift', value: day})}
-            />
-
+            <EmployeeForm />
             {/* Create button */}
             <CardItem>
                <Button onPress={this.onButtonPress.bind(this)}>
                   Create
                </Button>
             </CardItem>
-
          </Card>
       );
    }
 }
 
 const mapStateToProps = state => {
-   const { name, phone, shift } = state.employeeUpdate;
-   return { name, phone, shift };
+  const { name, phone, shift } = state.employeeUpdate;
+  return { name, phone, shift };
 };
 
-export default connect(mapStateToProps, { employeeUpdate, employeeCreate })(EmployeeCreate);
+export default connect(mapStateToProps, { employeeCreate })(EmployeeCreate);
